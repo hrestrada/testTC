cfcd6a7e9e769d0f00693cb255beee7c94d1965a
jhallos <67607041+jhallos@users.noreply.github.com>
2022-12-16 14:54:08 -0700

    Create testHTML4_params.js

---
+++ testHTML4_params.js
@@ -0,0 +1,47 @@
+var SFChatParamJS = SFChatParamJS || (function () {
+    // params for init embedded_svc
+    var siteUrl;
+    var instanceUrl;
+    var baseLiveAgentContentURL;
+    var deploymentId;
+    var buttonId;
+    var baseLiveAgentURL;
+    var eswLiveAgentDevName;
+    var companyId;
+    // end params for init embedded_svc
+    //var varProductID   = '020806'; //Needs to be gather from the URL
+    var varProjectID = '';
+    var varSkill1 = '';
+    var varSkill2 = '';
+    var varSkill3 = '';
+    var varSkill4 = '';
+    var varSkill5 = '';
+    var varSkill6 = '';
+    var varSkill7 = '';
+    var varSkill8 = '';
+    var varSkill9 = '';
+    var varSkill10 = '';
+    var varVisitorID = setVisitorID();
+    var varVisitorURL = window.location.href;
+    var varSegment = '';
+    var varSegmentParam = '';
+    //UxUi Variables      
+    var prechatBackgroundImgURL = '';
+    var smallCompanyLogoImgURL = '';
+    var avatarImgURL = '';
+    var waitingStateBackgroundImgURL = '';
+    var chatbotAvatarImgURL = '';
+    var defaultMinimizedText = '';
+    var disabledMinimizedText = '';
+    var offlineSupportMinimizedText = '';
+    var loadingText = '';
+    var widgetWidth = '';
+    var widgetHeight = '';
+    var widgetFontSize = '';
+    var qualifiedChat = '';
+    var qualifiedChat = "false";
+
+    console.log('varVisitorID Value: ' + varVisitorID);
+    console.log('varVisitorURL Value: ' + varVisitorURL);
+
+}
