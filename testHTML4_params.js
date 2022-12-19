ae981b1261faea98182c62d1f9516b8bb8d59107
jhallos <67607041+jhallos@users.noreply.github.com>
2022-12-19 10:16:28 -0700

    Update testHTML4_params.js

--- testHTML4_params.js
+++ testHTML4_params.js
@@ -1,49 +1,5 @@
-var SFChatParamJS = SFChatParamJS || (function () {
-    // params for init embedded_svc
-    var siteUrl;
-    var instanceUrl;
-    var baseLiveAgentContentURL;
-    var deploymentId;
-    var buttonId;
-    var baseLiveAgentURL;
-    var eswLiveAgentDevName;
-    var companyId;
-    // end params for init embedded_svc
-    //var varProductID   = '020806'; //Needs to be gather from the URL
-    var varProjectID = '';
-    var varSkill1 = '';
-    var varSkill2 = '';
-    var varSkill3 = '';
-    var varSkill4 = '';
-    var varSkill5 = '';
-    var varSkill6 = '';
-    var varSkill7 = '';
-    var varSkill8 = '';
-    var varSkill9 = '';
-    var varSkill10 = '';
-    var varVisitorID = setVisitorID();
-    var varVisitorURL = window.location.href;
-    var varSegment = '';
-    var varSegmentParam = '';
-    //UxUi Variables      
-    var prechatBackgroundImgURL = '';
-    var smallCompanyLogoImgURL = '';
-    var avatarImgURL = '';
-    var waitingStateBackgroundImgURL = '';
-    var chatbotAvatarImgURL = '';
-    var defaultMinimizedText = '';
-    var disabledMinimizedText = '';
-    var offlineSupportMinimizedText = '';
-    var loadingText = '';
-    var widgetWidth = '';
-    var widgetHeight = '';
-    var widgetFontSize = '';
-    var qualifiedChat = '';
-    var qualifiedChat = "false";
+//var SFChatParamJS = SFChatParamJS || (function () {
 
-    console.log('varVisitorID Value: ' + varVisitorID);
-    console.log('varVisitorURL Value: ' + varVisitorURL);
-    
     
     //CDWneedleParam.txt
         var needleParam = needleParam || {};
@@ -59,9 +15,9 @@ var SFChatParamJS = SFChatParamJS || (function () {
                         if (1) {
                             window.needleParam = window.needleParam || {};
                             needleParam.ExternalNetwork = (cdwTagManagementData.extranet !== undefined) ? cdwTagManagementData.extranet : "Not Defined on this page";
-                            needleParam.productId = cdwTagManagementData.product_id;
+                            needleParam.productId = '1148029' //cdwTagManagementData.product_id;
                             needleParam.categoryId = cdwTagManagementData.webclasscode_level1name + '|' + cdwTagManagementData.webclasscode_level2name + '|' + (cdwTagManagementData.webclasscode_level3name ? cdwTagManagementData.webclasscode_level3name : "N/A");
-                            needleParam.productPrice = cdwTagManagementData.product_price;
+                            needleParam.productPrice = '99.01' //cdwTagManagementData.product_price;
                             needleParam.inviteContext = cdwTagManagementData.product_category;
                             if (cdwTagManagementData.page_type.toLowerCase().indexOf("product_") > -1 && location.pathname.indexOf('compare') < 0) {
                                 needleParam.setBrand = cdwTagManagementData.product_root_brand_name ? cdwTagManagementData.product_root_brand_name : "";
@@ -302,4 +258,4 @@ var SFChatParamJS = SFChatParamJS || (function () {
         console.log('Needle', Needle);
     
 
-}());
+//}();
