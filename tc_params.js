d9dcc10875b4a24d146b6af4ec4d337579fe9322
jhallos <67607041+jhallos@users.noreply.github.com>
2023-01-06 13:46:02 -0700

    Update tc_params.js

--- tc_params.js
+++ tc_params.js
@@ -3,13 +3,13 @@
             (function(TCP) {
 
                             window.tcParam = window.tcParam || {};
-                            tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
-                            tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
-                            tcParam.setBrand = '/apple/i';
+                            //tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
+                            //tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
+                            //tcParam.setBrand = '/apple/i';
                             //tcParam.events = 'purchase';
-                            tcParam.orderId = '222233331';//cdwTagManagementData.levordref;
-                            tcParam.currencyCode = 'USD';
-                            tcParam.orderItems = 'Product1:2:400.00,Product2:1:200.00';//productString;
+                            //tcParam.orderId = '222233331';//cdwTagManagementData.levordref;
+                            //tcParam.currencyCode = 'USD';
+                            //tcParam.orderItems = 'Product1:2:400.00,Product2:1:200.00';//productString;
                             
                                 
         }(tcParam));
