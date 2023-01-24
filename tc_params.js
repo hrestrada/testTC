be45aa1342929d0569048eb5999a920e17b14196
hrestrada <116091615+hrestrada@users.noreply.github.com>
2023-01-24 12:42:03 -0500

    Update tc_params.js

--- tc_params.js
+++ tc_params.js
@@ -3,13 +3,14 @@
             (function(TCP) {
 
                             window.tcParam = window.tcParam || {};
-                            //tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
-                            //tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
+                            tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
+                            tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
                             tcParam.setBrand = '/apple/i';
-                            //tcParam.events = 'purchase';
-                            //tcParam.orderId = '222233331';//cdwTagManagementData.levordref;
-                            //tcParam.currencyCode = 'USD';
-                            //tcParam.orderItems = 'Product1:2:400.00,Product2:1:200.00';//productString;
+                            tcParam.events = 'purchase';
+                            tcParam.orderId = '222233331';//cdwTagManagementData.levordref;
+                            tcParam.currencyCode = 'USD';
+                            tcParam.orderItems = 'Product1:2:400.00,Product2:1:200.00';//productString;
+                            tcParam.programId = 'CDW1';
                             
                                 
         }(tcParam));
