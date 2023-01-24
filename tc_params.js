8b92f0565b6016dd6a33b3c595fe183b062b4e2b
hrestrada <116091615+hrestrada@users.noreply.github.com>
2023-01-24 17:44:35 -0500

    Update tc_params.js

--- tc_params.js
+++ tc_params.js
@@ -3,9 +3,9 @@
             (function(TCP) {
 
                             window.tcParam = window.tcParam || {};
-                          //  tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
-                            //tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
-                            //tcParam.setBrand = '/apple/i';
+                            tcParam.productId = '1148029'; //cdwTagManagementData.product_id;
+                            tcParam.productPrice = '99.01'; //cdwTagManagementData.product_price;
+                            tcParam.setBrand = '/apple/i';
                             tcParam.events = 'purchase';
                             tcParam.orderId = '222233331';//cdwTagManagementData.levordref;
                             tcParam.currencyCode = 'USD';
@@ -14,4 +14,4 @@
                             
                                 
         }(tcParam));
-        //console.log('tcParam ==> ', JSON.stringify(tcParam));
+        console.log('tcParam ==> ', JSON.stringify(tcParam));
